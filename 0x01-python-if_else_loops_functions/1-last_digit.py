3925b36d46f3d76d298fe18938fad3367c6c5c3a
sammieagb <gliddencop@gmail.com>
2023-06-10 04:33:37 -0700

    2nd

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
 import random
-numbr = random.randint(-10000, 10000)
-if numbr >= 0:
-    last_digit = numbr % 10
+number = random.randint(-10000, 10000)
+last_digit = abs(number) % 10
+if number < 0:
+    last_digit = -last_digit
+if last_digit > 5:
+    print(f"Last digit of {number :d} is {last_digit :d} and is greater \
+than 5")
+elif last_digit == 0:
+    print(f"Last digit of {number :d} is {last_digit :d} and is 0")
 else:
-    last_digit = ((-numbr % 10) * -1)
-
-message = f"Last digit of {numbr} is {last_digit}"
-
-if last_digit == 0:
-    print(f"{message} and is 0")
-elif last_digit > 5 and last_digit % 10 != 0:
-    print(f"{message} and is greater than 5")
-else:
-    print(f"{message} and is less than 6 and not 0")
+    print(f"Last digit of {number :d} is {last_digit :d} and is less \
+than 6 and not 0")
--- 0x01-python-if_else_loops_functions/10-add.py
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-# 10-add.py
-
 def add(k, j):
 """Return the addition of k & J."""
 return (k + j)
